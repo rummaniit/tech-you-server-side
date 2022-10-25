aa4841d04da45c8a417a1eaed93671a48e074d5f
rummaniit <rummanoffice12@gmail.com>
2022-10-25 13:36:30 +0600

    send request

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+app.use(cors())
+
+const courses = require('./data/courses.json')
+app.get('/', (req, res) => {
+    res.send(courses)
+})
