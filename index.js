0f6bb4e7a64cddea790ec5371b5192f21e996153
rummaniit <rummanoffice12@gmail.com>
2022-10-27 22:54:41 +0600

    final commit server

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 const courses = require('./data/courses.json')
+
+// app.get('/', (req, res) => {
+//     res.send('api is running')
+// })
+app.get('/', (req, res) => {
+    res.send('Api is running')
+})
+
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
